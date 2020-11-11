ecb70666ec762a1d2b338c48d124d3ce89c89c78
K1ll3rB14D <64766541+kill3rBi4d@users.noreply.github.com>
2020-11-11 12:11:47 +0100

    Update and rename ReadMe.md to ReadMe.js

---
+++ phones/flashpart/ReadMe.js
@@ -0,0 +1,8 @@
+//Comands
+//After activating  OEM and USB DEBUGGING on your device
+//Switch to your computer and open the ADB FastBoot and type the following commands
+
+>fastboot devices
+>fastboot flash recovery (Twrp name/file name).img
+//DONE!!
+//You can booot your device to the recovery mode and do whatever
